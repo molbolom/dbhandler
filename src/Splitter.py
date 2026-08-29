9f9a037c2776babe330dfb71b22bf7994e1e3392
molbolom <molbolom@gmail.com>
2026-08-29 00:56:14 -0400

    Fixed bugs with Splitter.split.

--- src/Splitter.py
+++ src/Splitter.py
@@ -40,70 +40,133 @@ class Splitter:
         self.spacers.append(spc)
 
 
-
     @classmethod
     def split(self, instr):
-
         A = []
 
         x = 0
 
+        head = -1
         tail = -1
+        discardstring = False
 
         while x < len(instr):
-            for sp in self.spacers:
-                if sp == instr[x]:
-                    sp = ""
-                    break;
-            if sp == "":
-                x+=1
-                continue
+            if head < 0:
+                head = x                    # Start read of string.
 
-            pos = [0,0]
-           
             for d in self.delms:
-                if d[0] == instr[x:x+d[1]]:
+                if d[0] == instr[x:x+d[1]]:                 # Found head.
                     tail = instr[x+d[1]:].find(d[2])
-                    if tail < 0:
-                        d = None
+                    if tail < 0:                            # Tail wasn't found, read normal string.
+                        break
+                    if head != x:                           # Delm is against string.
+                        tail=x                              # Set tail to last character.
+                        x-=1                                # Move x to previous character.
+                        break
                     else:
-                        tail = tail + x + d[1]
-
                         if d[4] & self.DISCARD_STRING:
-                            tail = tail + d[3] 
-                            break
-
+                            discardstring = True
                         if d[4] & self.DISCARD_DELMS:
-                            pos = [x+d[1], tail]
+                            tail -= 1
+                            head += d[1]
+                            x += tail + d[3]
+                            break 
                         else:
-                            pos = [x, tail+d[3]]
-                        x = x + d[3]
-                        print(pos, "   ", x, ":", tail)
-                    break
-                else:
-                    d = None
-
-            if d == None:
-                tail = x
-                while True:
-                    tail+=1
-                    if tail >= len(instr):
-                        pos = [x,tail]
-                        break
-                    for sp in self.spacers:
-                        if instr[tail] == sp:
-                            pos=[x, tail]
-                            sp = ""
+                            tail += x + d[1] + d[3] 
+                            x = tail
                             break
-                    if sp == "":
+
+            if tail < 0:
+                for sp in self.spacers:                     # Test for space characters. Non-readable characters.
+                    if instr[x] == sp:
+                        tail = x
                         break
-
-            x = tail+1
-
-            if pos[0] != pos[1]:
-               
-                A.append(instr[pos[0]:pos[1]])
+            if tail > 0:
+                if discardstring == False:
+                    A.append(instr[head:tail])
+                tail = -1
+                head = -1
+                discardstring = False
+            x+=1
+        if head != -1:
+            A.append(instr[head:x])
 
         return(A)
 
+#     @classmethod
+#     def split(self, instr):
+# 
+#         A = []
+# 
+#         x = 0
+# 
+#         tail = -1
+# 
+#         while x < len(instr):
+#             for sp in self.spacers:
+#                 if sp == instr[x]:
+#                     sp = ""
+#                     break;
+#             if sp == "":
+#                 x+=1
+#                 continue
+# 
+#             pos = [0,0]
+#            
+#             for d in self.delms:
+#                 if d[0] == instr[x:x+d[1]]:
+#                     tail = instr[x+d[1]:].find(d[2])
+#                     if tail < 0:
+#                         d = None
+#                     else:
+#                         tail = tail + x + d[1]
+# 
+#                         if d[4] & self.DISCARD_STRING:
+#                             tail = tail + d[3] 
+#                             break
+# 
+#                         if d[4] & self.DISCARD_DELMS:
+#                             pos = [x+d[1], tail]
+#                         else:
+#                             pos = [x, tail+d[3]]
+#                         x = x + d[3]
+#                         print(pos, "   ", x, ":", tail)
+#                     break
+#                 else:
+#                     d = None
+# 
+#             if d == None:
+#                 tail = x
+#                 while True:
+#                     tail+=1
+#                     if tail >= len(instr):
+#                         pos = [x,tail]
+#                         break
+#                     for sp in self.spacers:
+#                         if instr[tail] == sp:
+#                             pos=[x, tail]
+#                             sp = ""
+#                             break
+#                     if sp == "":
+#                         break
+# 
+#             x = tail+1
+# 
+#             if pos[0] != pos[1]:
+#                
+#                 A.append(instr[pos[0]:pos[1]])
+# 
+#         return(A)
 
+Splitter.new_delm("\"", "\"")
+Splitter.new_delm("[", "]")
+Splitter.new_delm("{", "}")
+Splitter.new_delm("'", "'")
+Splitter.new_delm("##", "##", flags = Splitter.DISCARD_STRING)
+
+while True:
+    a = input("Input a string $ ")
+    if a == "quit":
+        break
+    S = Splitter.split(a)
+    print(S)
